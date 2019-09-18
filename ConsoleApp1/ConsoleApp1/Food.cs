4afcc164f37d9dc5ccc576619b5c5d36b2ebf0ed
Karmo <frozyice@gmail.com>
2019-09-18 12:53:32 +0300

    clear

---
+++ ConsoleApp1/ConsoleApp1/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Food
+    {
+        public String Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Food (string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Food> groceries = new List<Food>
+            {
+                new Food("banana", 1.2),
+                new Food("apple", 2.3),
+                new Food("icecream",2.5)
+            };
+
+            Console.WriteLine("Please enter Your firstname: ");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("Hi, "+firstname+"!");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter Your lastname: ");
+            string lastname = Console.ReadLine();
+
+            Person client = new Person(firstname, lastname);
+
+            Console.WriteLine("Welcome to our shop, " + client + "!");
+
+            client.ShoppingCart = new ShoppingCart();
+            Console.WriteLine("What do you want to buy?");
+
+            string foodName = Console.ReadLine();
+
+            Food chosenFood = groceries.FirstOrDefault(x => x.Name == foodName);
+            if (chosenFood == null)
+            {
+                Console.WriteLine("we dont have "+foodName);
+            }
+            else
+            {
+                Console.WriteLine("How much?");
+            }
+        }
+    }
+}
