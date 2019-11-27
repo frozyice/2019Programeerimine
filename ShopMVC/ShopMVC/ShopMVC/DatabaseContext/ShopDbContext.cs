21edf67d985194afa9bc72b68363c4116a1c30a6
Karmo <Karmo.Kiima@Eesti.ee>
2019-11-27 10:26:43 +0200

    Add dbContext

---
+++ ShopMVC/ShopMVC/ShopMVC/DatabaseContext/ShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ShopMVC.DatabaseContext
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
